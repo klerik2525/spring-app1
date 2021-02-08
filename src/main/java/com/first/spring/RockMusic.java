ab9a06d320ba38b11c99a986844b2816533074ca
klerik2525 <64359472+klerik2525@users.noreply.github.com>
2021-02-08 18:26:14 +0300

    refact to autoscan annotations

--- src/main/java/com/first/spring/RockMusic.java
+++ src/main/java/com/first/spring/RockMusic.java
@@ -1,6 +1,9 @@
 package com.first.spring;
 
-public class RockMusic implements Music{
+import org.springframework.stereotype.Component;
+
+@Component
+public class RockMusic implements Music {
     @Override
     public String getSong() {
         return "Wind cries Mary";
